72f37206175f5dbf7e32f7c7807a6db17f4dc672
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-13 10:57:25 +0300

    Добавил решение задачи номер 1

--- 1Task/Program.cs
+++ 1Task/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Выяснить являются ли три числа сторонами треугольника
+
+int GetNumber()
+{
+    Console.WriteLine("Введите число :");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int number1 = GetNumber();
+int number2 = GetNumber();
+int number3 = GetNumber();
+
+if(number1+number2>number3 && number2+number3>number1 && number1+number3>number2)
+{
+    Console.WriteLine("Это может быть треугольником");
+}
+else 
+Console.WriteLine("Это не может быть треугольником");
