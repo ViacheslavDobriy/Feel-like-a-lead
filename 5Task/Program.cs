ec4ad3ff8877f8430bd8c534a18b437e9d303927
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-14 14:25:29 +0300

    Добавил задачу на вывод чисел Фибоначе на экран

---
+++ 5Task/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать числа Фибоначчи
+
+int GetNumber()
+{
+    Console.WriteLine("Сколько чисел из ряда Фибоначе вым показать?");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+int Fibonache(int number)
+{
+    if (number==1 || number==2 ) return 1;
+    else return Fibonache(number-1)+Fibonache(number-2);
+}
+void ShowFibonache(int number)
+{
+    for(int i = 1; i<=number; i++)
+    {
+        Console.Write($"{Fibonache(i)} ");
+    }
+}
+int Limit = GetNumber();
+ShowFibonache(Limit);
