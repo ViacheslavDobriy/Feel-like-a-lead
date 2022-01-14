a9bb5a2d3008a1fd5634b17bf9e8557fe76cbc6d
Vyacheslav <dobrov.slava@yandex.ru>
2022-01-14 15:24:53 +0300

    Реализовал 4 метода, благодаря которым программа выдает полностью скопированный массив, первый массив был введен с клавиатуры пользователем

---
+++ 6Task/Program.cs
@@ -0,0 +1,38 @@
+﻿// Написать программу копирования массива
+
+int GetSizeArray()
+{
+    Console.WriteLine("Введите количество чисел, которое вы хотите ввести с клавиатуры : ");
+    int number = Convert.ToInt32(Console.ReadLine());
+    return number;
+}
+void FillArray(int[] Array)
+{
+    for(int i = 0 ; i < Array.Length ; i++)
+    {
+        Console.WriteLine($"Введите {i+1} число: ");
+        Array[i] = Convert.ToInt32(Console.ReadLine());
+    } 
+}
+void ShowLine(int[] Array)
+{
+    for(int i = 0 ; i <Array.Length; i++)
+    Console.Write($"{Array[i]} ");
+    Console.WriteLine();
+}
+int[] CreateArray(int[] Array)
+{
+    int[] NewArray = new int[Array.Length];
+    for(int i = 0; i < Array.Length; i++)
+    {
+        NewArray[i]=Array[i];
+    }
+    return NewArray;
+}
+int number = GetSizeArray();
+int[] OldArray = new int[number];
+FillArray(OldArray);
+ShowLine(OldArray);
+int[] NewArray = CreateArray(OldArray);
+Console.WriteLine("____________________________");
+ShowLine(NewArray);
